6622b9ebd337a96fd795e3a2668a60c697699edf
Liamthwin <134946485+Liamthwin@users.noreply.github.com>
2023-07-27 20:26:09 +0630

    Add files via upload
    python_project

---
+++ liam.py
@@ -0,0 +1,24 @@
+def Q_one():
+    ask=input("Are u sure to answer?: ")
+    if ask=="yes"or ask=="Yes":
+        question1()
+def question1():
+    quest=input("What is 8+8?")
+    if quest=="16":
+        question2()
+def question2():
+    quest1=input("What is 9*2?")
+    if quest1=="18":       
+        question3()
+def question3():
+    quest2=input("What is 10-10?")
+    if quest2=="0":
+     question1()
+Q_one()
+
+
+    
+
+
+
+
