d93e4b55b74f84a57acd92146a4248db70a93f3d
Thwin Thant <thuliam163@gmail.com>
2023-09-04 20:13:04 +0630

    tkinter project

--- liam.py
+++ liam.py
@@ -1,31 +1,44 @@
-import os
-def Q_one():
-    ask=input("Are u sure to answer?: ")
-    if ask=="yes"or ask=="Yes":
-        question1()
-def question1():
-    quest=input("What is 8+8?")
-    if quest=="16":
-        question2()
-def question2():
+import tkinter as tk
+window = tk.Tk()
+greeting = tk.Label(text="Kaung Htet Kyaw school", foreground="red", background="black")
+greeting.pack()
 
-    quest1=input("What is 9*2?")
-    if quest1=="18":       
-        question3()
-def question3():
-    quest2=input("What is 10-10?")
-    if quest2=="0":
-     question1()
-def question4():
-    quest3=input("What is 10-8?")
-    if quest3=="2":
-        question1()
- def question5():
-     quest4=input("what is 30-10")
-     if quest4=="20":
-         question1()
-Q_one()
+myfirst = label = tk.Label(text="Sign up", width=10, height=5, )
+myfirst.pack()
 
+first_name = tk.Entry()
+first_name.pack()
+print(first_name.get())
+
+last_name = tk.Entry()
+last_name.pack()
+
+gmail = tk.Entry()
+gmail.pack()
+
+password = tk.Entry()
+password.pack()
+
+Gender = tk.Entry()
+Gender.pack()
+def get_first_name():
+    print("First name:", first_name.get())
+    print("Last name:", last_name.get())
+    print("Gmail:", gmail.get())
+    print("Password:", password.get())
+    print("Gender:", Gender.get())
+
+button = tk.Button(
+    text="Sign up",
+    width=25,
+    height=5,
+    bg="blue",
+    fg="yellow",
+    command=get_first_name,
+)
+button.pack()
+
+window.mainloop()
 
 
     
