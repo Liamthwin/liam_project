476b36cf17cfc13a0f9466950ae3909175c050e4
Thwin Thant <thuliam163@gmail.com>
2023-09-15 20:01:46 +0630

    add new code

--- liam.py
+++ liam.py
@@ -1,42 +1,40 @@
-import tkinter as tk
-window = tk.Tk()
-greeting = tk.Label(text="Kaung Htet Kyaw school", foreground="red", background="black")
-greeting.pack()
+from tkinter import*
+window = Tk()
+window.geometry("500x200")
+def textbox2():
+    textbox2=Message(text=data1.get())
+    textbox2.place(x=155,y=70,width=200,height=25)
+    textbox2["bg"]="white"
+    textbox2["fg"]="black"
 
-myfirst = label = tk.Label(text="Sign up", width=10, height=5, )
-myfirst.pack()
+def textbox3():
+    textbox3=Message(text=data2.get())
+    textbox3.place(x=155,y=100,width=200,height=25)
+    textbox3["bg"]="white"
+    textbox3["fg"]="black"
 
-first_name = tk.Entry()
-first_name.pack()
-print(first_name.get())
+data1=StringVar()
+label1=Label(text="Enter Your Name: ")
+label1.place(x=30,y=20)
+textbox1=Entry(textvariable=data1)
+textbox1.place(x=150,y=20,width=200,height=25)
+textbox1["justify"]="center"
 
-last_name = tk.Entry()
-last_name.pack()
+data2=StringVar()
+label2=Label(text="Enter Your Age: ")
+label2.place(x=30,y=40)
+textbox5=Entry(textvariable=data2)
+textbox5.place(x=150,y=40,width=200,height=25)
+textbox5["justify"]="center"
 
-gmail = tk.Entry()
-gmail.pack()
 
-password = tk.Entry()
-password.pack()
+textbox1.focus()
+button=Button(text="Press Me",command=textbox2)
+button.place(x=30,y=80,width=120,height=25)
 
-Gender = tk.Entry()
-Gender.pack()
-def get_first_name():
-    print("First name:", first_name.get())
-    print("Last name:", last_name.get())
-    print("Gmail:", gmail.get())
-    print("Password:", password.get())
-    print("Gender:", Gender.get())
-
-button = tk.Button(
-    text="Sign up",
-    width=25,
-    height=5,
-    bg="blue",
-    fg="yellow",
-    command=get_first_name,
-)
-button.pack()
+textbox5.focus()
+button=Button(text="Press Me",command=textbox3)
+button.place(x=30,y=100,width=120,height=25)
 
 window.mainloop()
 
